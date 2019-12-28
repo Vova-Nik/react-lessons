1e6de8c3c0050e3f465bca81315686e88863fdbf
Vova-Nik <vmaseich@gmail.com>
2019-12-28 08:39:50 +0200

    Lesson 13

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './pngfuel.png';
+import './App.css';
+
+const Header = () => {
+    return <header className="header">
+        <img src={logo} alt="Smiley dog" height="64"></img>
+    </header>
+}
+export default Header;
